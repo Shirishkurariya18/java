c4e61ecf6796785cbbbfe5f07fecbf2416cc4708
shirish kurariya <harshkurariya2005@gmail.com>
2025-04-22 22:02:08 +0530

    added string.java for string operations

---
+++ string.java
@@ -0,0 +1,26 @@
+// string print line vise
+
+// public class string {
+//     // string is called as set of charecters
+//     public static void main(String[] args) {
+//         String str = "hello world";
+//         //  System.out.println(str);
+//         for (int i = 0; i < str.length();i++) {
+//             System.out.println(str.charAt(i));
+//         }
+//     }
+// }
+
+// reverse string line vise
+public class string {
+  
+    public static void main(String[] args) {
+        String str = "hello world";
+        //  System.out.println(str);
+        for (int i = str.length()-1; i>=0;  i--) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
+
+// 
