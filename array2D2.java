a6e73261a69cebd0881ed4c3e3e1e20c0909a6eb
shirish kurariya <harshkurariya2005@gmail.com>
2025-04-22 21:54:52 +0530

    Added array2D2.java for 2D operations

---
+++ array2D2.java
@@ -0,0 +1,58 @@
+//sum of two dimension array 
+
+
+// public class array2D2 {
+//     public static void main(String[] args) {
+//         int arr1[][] = {{1,2,4},{1,2,4},{1,4,2}};
+//        int arr2[][] = {{1,2,3},{1,2,3},{1,8,6}};
+//       int sum = 0;
+//       for (int i = 0; i < arr1.length; i++) {
+//         for (int j = 0; j < arr1[i].length; j++) {
+//           sum += arr1[i][j] + arr2[i][j]; 
+//         }
+//       }
+//       System.out.println("sum of 2 dimension array elements : " + sum);
+//     }
+// }
+
+
+// public class array2D2 {
+//   public static void main(String[] args) {
+//       int arr1[][] = {{1,2,4},{1,2,4},{1,4,2}};
+//      int arr2[][] = {{1,2,3},{1,2,3},{1,8,6}};
+//      int arr3[][] = {{1,5,7},{9,8,2},{0,0,0}};
+//     int sum = 0;
+//     for (int i = 0; i < arr1.length; i++) {
+//       for (int j = 0; j < arr1[i].length; j++) {
+//         sum += arr1[i][j] + arr2[i][j] + arr3[i][j]; 
+//       }
+//     }
+//     System.out.println("sum of 2 dimension array elements : " + sum);
+//   }
+// }
+
+import java.util.Scanner;
+public class array2D2 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner (System.in);
+    System.out.println("enter the no of rows : ");
+    int rows = in.nextInt();
+    System.out.println("enter the no of column : ");
+    int column = in.nextInt();
+    int[][] arr = new int[rows][column];
+
+    System.out.println("enter the values of array : ");
+    int sum = 0;
+    for (int i = 0; i < rows; i++) {
+      for(int j = 0; j < column; j++){
+
+        arr[i][j] = in.nextInt();
+        sum += arr[i][j];
+      }
+  }
+  System.out.println("----------------------------");
+  System.out.println(sum);
+}
+}
+
+
